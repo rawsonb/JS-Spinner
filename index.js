605a201e6ae1eb995e2dd95cc328dc5dcda19568
f.blackmon <f.rawsonblackmon@gmail.com>
2022-11-21 17:44:01 -0500

    add bg

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 let app = new PIXI.Application({
-    width: 640, 
-    height: 360, backgroundColor: 0xaaaa00
+    width: 800, 
+    height: 800, backgroundColor: 0x055099
 });
 
 document.body.appendChild(app.view);
 
 
-const numSlices = 10;
+const numSlices = 6;
 
 let slices = [];
 
 
 let sliceGx = new PIXI.Graphics();
+sliceGx.beginFill(0x000000);
+sliceGx.drawCircle(0,0, Math.min(app.screen.width, app.screen.height)/2.5 + 5);
+let spinnerBgTx = app.renderer.generateTexture(sliceGx);
+let spinnerBg = new PIXI.Sprite(spinnerBgTx);
+spinnerBg.anchor.set(0.5);
+spinnerBg.x = app.screen.width/2;
+spinnerBg.y = app.screen.height/2;
+app.stage.addChild(spinnerBg);
+
 
 for (let i = 0; i < numSlices; i++){
     let color = Math.random() * 0xFFFFFF;
@@ -28,13 +37,13 @@ for (let i = 0; i < numSlices; i++){
     if(numSlices == 2){
         sliceSprite.anchor.set(0.5, 0);
     } else if(numSlices == 3){
-        sliceSprite.anchor.set(0.334,0.003);
+        sliceSprite.anchor.set(1/3,0.00);
     } else{
-        sliceSprite.anchor.set(0.002,0.002);
+        sliceSprite.anchor.set(0.0,0.0);
     }
     sliceSprite.x = app.screen.width/2;
     sliceSprite.y = app.screen.height/2;
-    sliceSprite.scale.set(1.005, 1.005);
+    sliceSprite.scale.set(1, 1.00);
     
     sliceSprite.rotation = 2*Math.PI/numSlices * i;
     app.stage.addChild(sliceSprite);
